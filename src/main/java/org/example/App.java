8af7d1ea9c3fe4208f85e70cf2a846a15b300248
hemag <hemaghg@gmail.com>
2020-10-06 14:31:11 +0200

    Spring basic code part1

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,30 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(ComponentScanConfig.class);
+        System.out.println( "Welcome Spring!" );
+
+
+        StudentManagement studentManagement = context.getBean(StudentManagement.class);
+
+        System.out.println("Name a new student:");
+        studentManagement.save(studentManagement.create());
+        System.out.println("and one more student:");
+        studentManagement.save(studentManagement.create());
+
+        System.out.println("Searching for student with id 1002");
+        System.out.println(studentManagement.find(1002));
+
+        System.out.println("And here is all students:");
+        studentManagement.findAll().forEach(s-> System.out.println(s));
+
+
+    }
